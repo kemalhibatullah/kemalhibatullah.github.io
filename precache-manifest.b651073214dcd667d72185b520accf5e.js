38752740d0c323226e2e1cbef6efd085a23849f7
kemalhibatullah <43361071+kemalhibatullah@users.noreply.github.com>
2019-05-21 08:23:04 +0700

    Updates

---
+++ precache-manifest.b651073214dcd667d72185b520accf5e.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192b870ca3acf36909bb8d8a5a0d580e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad07a84b9c41993b23a4",
+    "url": "/static/css/main.d0370b53.chunk.css"
+  },
+  {
+    "revision": "b26f2cbd4436c722826e",
+    "url": "/static/js/2.1a6cec39.chunk.js"
+  },
+  {
+    "revision": "ad07a84b9c41993b23a4",
+    "url": "/static/js/main.ad38cff2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86c0f065570bb1b37347597b964d889c",
+    "url": "/static/media/K.86c0f065.webp"
+  },
+  {
+    "revision": "9fa0affbe0a6310cf3fc4aaec9c42b45",
+    "url": "/static/media/dribbble.9fa0affb.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "88e20fb0d84fee510326a84bf9a84c76",
+    "url": "/static/media/github.88e20fb0.png"
+  },
+  {
+    "revision": "342b2a6112eabc4861ad339588df3c7c",
+    "url": "/static/media/hero1.342b2a61.webp"
+  },
+  {
+    "revision": "be9bf65e5890112f257e50a153b56cac",
+    "url": "/static/media/hero2.be9bf65e.webp"
+  },
+  {
+    "revision": "5cdf3c67c33957f3656b8f895f6eb3f4",
+    "url": "/static/media/hero3.5cdf3c67.webp"
+  },
+  {
+    "revision": "a2e3920f82ef0a20ec9df29d29bd3b9a",
+    "url": "/static/media/ig2.a2e3920f.webp"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "c42a8f28ba787d543b13e58278bc8cae",
+    "url": "/static/media/img2.c42a8f28.webp"
+  },
+  {
+    "revision": "1a87d7146cdef49766265b2122a1a3e2",
+    "url": "/static/media/img3.1a87d714.webp"
+  },
+  {
+    "revision": "31ff9b577c83ddecab3c53441144a778",
+    "url": "/static/media/img4.31ff9b57.webp"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "cd8a3102bd948303024fa8285513df4d",
+    "url": "/static/media/studio1.cd8a3102.jpg"
+  },
+  {
+    "revision": "6062b60fa58a038c28c23cd9aeb13a43",
+    "url": "/static/media/studio2.6062b60f.jpg"
+  },
+  {
+    "revision": "afbd00ce9637d8f2dcf0248aa533409e",
+    "url": "/static/media/studio3.afbd00ce.png"
+  },
+  {
+    "revision": "ae20797f8af6904769cbc84d4fdce6be",
+    "url": "/static/media/studio4.ae20797f.png"
+  },
+  {
+    "revision": "2481973d5edd658016b7123f81c50e2a",
+    "url": "/static/media/studio5.2481973d.png"
+  },
+  {
+    "revision": "22949b1723e374e18d85faa61ff1a763",
+    "url": "/static/media/studio6.22949b17.png"
+  },
+  {
+    "revision": "0fc63ad64d4555986f84527181aa7b98",
+    "url": "/static/media/studio7.0fc63ad6.png"
+  },
+  {
+    "revision": "c11f024aaa5e1859c26fa1b6808b7194",
+    "url": "/static/media/xd.c11f024a.webp"
+  }
+]);
